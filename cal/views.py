a09c75230b587a3d4f0bb057fc5fde1792a24901
MinalMane <minal.july31@gmail.com>
2020-03-23 12:36:53 +0530

    added 5

--- cal/views.py
+++ cal/views.py
@@ -13,5 +13,5 @@ def home (request):
 def add (request):
     val1 = request.GET['num1']
     val2 = request.GET['num2']
-    res = val1 + val2 + 0
+    res = val1 + val2 + 5
     return render(request, "result.html",{'result': res})
